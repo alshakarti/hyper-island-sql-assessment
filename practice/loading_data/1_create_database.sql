2444b1f201c4a6451b24a03e56832ca00fb9905e
alshakarti <muhammad.alshakarti@icloud.com>
2025-03-12 14:41:23 +0100

    uploading files

---
+++ practice/loading_data/1_create_database.sql
@@ -0,0 +1,2 @@
+-- DROP DATABASE
+CREATE DATABASE top_skills;
---
+++ practice/querys/2_top_paying_jobs_skills.sql
@@ -0,0 +1,33 @@
+-- Question: What are the highest-paying data analyst jobs?
+-- Key insight 1: Identify the top 10 highest-paying data analyst roles
+-- Key insight 2: Identify the companies that offer the highest-paying data analyst roles
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        salary_year_avg,
+        name AS company_name
+    FROM
+        job_postings_fact
+    LEFT JOIN
+        company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10)
+
+SELECT
+    top_paying_jobs.*,
+    skills
+FROM
+    top_paying_jobs
+INNER JOIN
+        skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN
+        skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
